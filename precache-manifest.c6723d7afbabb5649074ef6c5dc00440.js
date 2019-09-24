3cb3d73a5f7e3268547d14bd5d6a60cba5e3c0ec
uxbear <yuekkuma@uxbear.com>
2019-09-24 11:24:30 +0900

    Updates

---
+++ precache-manifest.c6723d7afbabb5649074ef6c5dc00440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c285e0afd9233ef6e56387d5170f5ac",
+    "url": "/uxbear/react-sample.git/index.html"
+  },
+  {
+    "revision": "a4cd72b809b5410574e3",
+    "url": "/uxbear/react-sample.git/static/css/main.7398d0ad.chunk.css"
+  },
+  {
+    "revision": "18c4ad70270165ae2976",
+    "url": "/uxbear/react-sample.git/static/js/2.27b854ff.chunk.js"
+  },
+  {
+    "revision": "a4cd72b809b5410574e3",
+    "url": "/uxbear/react-sample.git/static/js/main.af6a2df8.chunk.js"
+  },
+  {
+    "revision": "6d6aaacd3a9e6e81a7f3",
+    "url": "/uxbear/react-sample.git/static/js/runtime-main.f88a327b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/uxbear/react-sample.git/static/media/logo.25bf045c.svg"
+  }
+]);
